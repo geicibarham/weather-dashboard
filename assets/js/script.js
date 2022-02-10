1c652918c40275d1f04ac40a8c5fcde94faacdce
geicibarham <geicianecosta43@gmail.com>
2022-02-10 14:20:23 -0500

    current weather function works fine now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,69 @@
 var formEl = document.querySelector("#city-form");
+var searchInput = document.querySelector("#city-input");
 var weatherContainerEl = document.querySelector("#current-weather-container");
+var currentcityname = document.querySelector("#current-city-name")
 var searchButtonEl = document.querySelector("#search-button");
-
-
-//var for current date to display on page
-var currentDate = moment().format('L');
-$("#current-date").text("(" + currentDate + ")");
-
+var temperature = document.querySelector("#temperature");
+var humidity = document.querySelector("#humidity");
+var windspeed = document.querySelector("#wind-speed");
+var uvindex = document.querySelector("#uv-index");
+var locationIcon = document.querySelector(".weather-icon");
+var currentDate = document.querySelector("#currentDate");
 
 //function to handle form submission
 var formSubmitHandler = function (event) {
     //prevent browser from automatically refreshing
-    event.preventDefault();
 
-    //get city from input element
-}
+
+};
+
 
 //create function to get the current weather on a specific city - fetch will be in here
 
 var getCurrentWeather = function () {
-    
+    event.preventDefault();
 
     var ApiKey = 'ee601a5be4293bbbbc2b2665840ba595';
-    var searchInput = $("#city-input").value;
+    var searchInput = $("#city-input").val();
 
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + ApiKey + "&units=imperial";
 
-    // https://api.openweathermap.org/data/2.5/weather?q=curitiba&appid=ee601a5be4293bbbbc2b2665840ba595&units=imperial
-    // var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q' + cityInput.value + '&appid=ee601a5be4293bbbbc2b2665840ba595' + '&units=imperial';
-
-    // var apiUrlat = `api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${ApiKey}`
-    // `https://api.openweathermap.org/data/2.5/weather?q=durham&appid=${ApiKey}`
 
     fetch(apiUrl)
         .then(function (response) {
-            response.json().then(function (response) {
-                console.log(response);
+            response.json().then(function (data) {
+               
+
+                const date = moment().format("MMMM Do YYYY");
+              
+                
+                //grab data and display
+                // locationIcon.innerHTML = data.weather[0];
+                // currentDate.innerHTML = date;
+                currentDate.innerHTML = date;
+                currentcityname.innerHTML = data.name;
+                console.log(data.main);
+                temperature.innerHTML = data.main.temp;
+                humidity.innerHTML = data.main.humidity;
+                windspeed.innerHTML = data.wind.speed;
+
+
+                console.log(data.weather[0]);
+                // uvindex
+
+                //  let icon = $("<img>").addClass("rounded mx-auto d-block");
+                // icon.attr("src", "http://openweathermap.org/img/wn/" + today.weather[0].icon + "@2x.png");
+
             });
         })
 
+}
+
+var displayCurrentweather = function () {
+    //weather main
 
 }
 
-// getCurrentWeather();
 
 formEl.addEventListener('submit', getCurrentWeather);
 
