1a442170c0248960d0a6ff476fda5d00c5a9a5d8
geicibarham <geicianecosta43@gmail.com>
2022-02-14 09:11:30 -0500

    displays 5 days forecast yay

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,3 +1,7 @@
+//things to do
+//01 - display icon
+// 05 - save local storage
+
 
 var formEl = document.querySelector("#city-form");
 var searchInput = document.querySelector("#city-input");
@@ -8,47 +12,38 @@ var humidity = document.querySelector("#humidity");
 var windspeed = document.querySelector("#wind-speed");
 var uvindex = document.querySelector("#uv-index");
 var currentDate = document.querySelector("#currentDate");
-var imgEl = document.querySelector("#weather-image");
 
 
 // 5 days forecast - 
 var dateDayone = document.querySelector("#date1");
-var iconOneEl = document.querySelector("#weather-image1");
 var dayOnetemperature = document.querySelector("#temperature1");
 var dayOnetempmin = document.querySelector("#temperature1min");
 var dayOnehumidity = document.querySelector("#humidity1");
 var dayOnewindSpeed = document.querySelector("#wind-speed1");
 
 var dateDaytwo = document.querySelector("#date2");
-var iconTwoEl = document.querySelector("#weather-image2");
 var dayTwotemperature = document.querySelector("#temperature2");
 var dayTwotempmin = document.querySelector("#temperature2min");
 var dayTwohumidity = document.querySelector("#humidity2");
 var dayTwowindSpeed = document.querySelector("#wind-speed2");
 
 var dateDaythree = document.querySelector("#date3");
-var iconthreeEl = document.querySelector("#weather-image3");
 var dayThreetemperature = document.querySelector("#temperature3");
 var dayThreetempmin = document.querySelector("#temperature3min");
 var dayThreehumidity = document.querySelector("#humidity3");
 var dayThreewindSpeed = document.querySelector("#wind-speed3");
 
 var dateDayfour = document.querySelector("#date4");
-var iconFourEl = document.querySelector("#weather-image4");
 var dayFourtemperature = document.querySelector("#temperature4");
 var dayFourtempmin = document.querySelector("#temperature4min");
 var dayFourhumidity = document.querySelector("#humidity4");
 var dayFourwindSpeed = document.querySelector("#wind-speed4");
 
-
 var dateDayfive = document.querySelector("#date5");
-var iconFiveEl = document.querySelector("#weather-image5");
 var dayFivetemperature = document.querySelector("#temperature5");
 var dayFivetempmin = document.querySelector("#temperature5min");
 var dayFivehumidity = document.querySelector("#humidity5");
 var dayFivewindSpeed = document.querySelector("#wind-speed5");
-
-var deletebutton = document.querySelector("#delete-button");
 
 
 var getCurrentWeather = function () {
@@ -61,7 +56,7 @@ var getCurrentWeather = function () {
         alert("Please enter a city name to get started!");
     }
 
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + ApiKey + "&units=imperial";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&Appid=" + ApiKey + "&units=imperial";
 
 
     fetch(apiUrl)
@@ -77,7 +72,7 @@ var getCurrentWeather = function () {
                 temperature.innerHTML = data.main.temp + "°F";
                 humidity.innerHTML = data.main.humidity + " " + "%";
                 windspeed.innerHTML = data.wind.speed + " " + "Mph";
-                imgEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
+
 
                 // get current lat and lon to get 5 days weather
                 var currentLat = data.coord.lat;
@@ -90,7 +85,6 @@ var getCurrentWeather = function () {
                     .then(function (response) {
                         response.json().then(function (data) {
                             //   add current uv index
-                            consoleData(data)
                             uvindex.innerHTML = data.current.uvi;
 
                             if (data.current.uvi <= 3) {
@@ -112,9 +106,6 @@ var getCurrentWeather = function () {
                             // populate each day 
 
                             dateDayone.innerHTML = travelTime1;
-
-
-                            iconOneEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + "@2x.png")
                             dayOnetemperature.innerHTML = data.daily[1].temp.day + "°F";
                             dayOnetempmin.innerHTML = data.daily[1].temp.min + "°F";
                             dayOnehumidity.innerHTML = data.daily[1].humidity + "%";
@@ -124,9 +115,6 @@ var getCurrentWeather = function () {
                             var travelTime2 = moment().add(2, 'day').format('M/D/YYYY');
 
                             dateDaytwo.innerHTML = travelTime2;
-
-
-                            iconTwoEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + "@2x.png")
                             dayTwotemperature.innerHTML = data.daily[2].temp.day + "°F";
                             dayTwotempmin.innerHTML = data.daily[2].temp.min + "°F";
                             dayTwohumidity.innerHTML = data.daily[2].humidity + "%";
@@ -136,8 +124,6 @@ var getCurrentWeather = function () {
 
                             var travelTime3 = moment().add(3, 'day').format('M/D/YYYY');
 
-
-                            iconthreeEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + "@2x.png")
                             dateDaythree.innerHTML = travelTime3;
                             dayThreetemperature.innerHTML = data.daily[3].temp.day + "°F";
                             dayThreetempmin.innerHTML = data.daily[3].temp.min + "°F";
@@ -149,7 +135,6 @@ var getCurrentWeather = function () {
                             var travelTime4 = moment().add(4, 'day').format('M/D/YYYY');
 
                             dateDayfour.innerHTML = travelTime4;
-                            iconFourEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + "@2x.png")
                             dayFourtemperature.innerHTML = data.daily[4].temp.day + "°F";
                             dayFourtempmin.innerHTML = data.daily[4].temp.min + "°F";
                             dayFourhumidity.innerHTML = data.daily[4].humidity + "%";
@@ -159,13 +144,25 @@ var getCurrentWeather = function () {
                             var travelTime5 = moment().add(5, 'day').format('M/D/YYYY');
 
                             dateDayfive.innerHTML = travelTime5;
-                            iconFiveEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.daily[5].weather[0].icon + "@2x.png")
                             dayFivetemperature.innerHTML = data.daily[5].temp.day + "°F";
                             dayFivetempmin.innerHTML = data.daily[5].temp.min + "°F";
                             dayFivehumidity.innerHTML = data.daily[5].humidity + "%";
                             dayFivewindSpeed.innerHTML = data.daily[5].wind_speed + " " + "Mph";
 
-                            saveToLocal(searchInput)
+
+                            //save to local storage
+                            searchedCities = [];
+
+                            var searchedCities = searchInput;
+                            localStorage.setItem("city", searchedCities);
+                            localStorage.getItem("city");
+
+                            if (searchedCities < 8) {
+                                searchedCities.forEach
+                            }
+
+
+
                         })
                     })
 
@@ -175,70 +172,6 @@ var getCurrentWeather = function () {
         })
 
 }
-
-function saveToLocal(searchInput) {
-    //save to local storage
-    var searchedCities = [];
-
-    if (localStorage.getItem("city")) {
-        // this is a string
-        // parse converts it back into an object
-        searchedCities = JSON.parse(localStorage.getItem("city"))
-    }
-
-    // var searchedCities = searchInput;
-
-    if (searchedCities.length < 6) {
-        searchedCities.push(searchInput)
-    } else {
-        searchedCities.splice(0, 1)
-        searchedCities.push(searchInput)
-    }
-
-    localStorage.setItem("city", JSON.stringify(searchedCities));
-    // localStorage.getItem("city");
-    renderHistory()
-}
-
-function consoleData(parameter) {
-    console.log(parameter)
-}
-
-function renderHistory() {
-    var historyEl = document.querySelector("#cityArray");
-    historyEl.innerHTML = ""
-
-    var history = JSON.parse(localStorage.getItem("city")) || [];
-
-    if (history.length == 0) {
-        var alertMessage = document.createElement("p")
-        alertMessage.setAttribute("class", "h4a");
-        alertMessage.textContent = "No History yet."
-        historyEl.append(alertMessage)
-
-    } else {
-        for (i = 0; i < history.length; i++) {
-            var link = document.createElement("a");
-            var newButton = document.createElement("button");
-            newButton.textContent = history[i];
-            link.setAttribute("href", history[i].html_url);
-            newButton.setAttribute("class", "general-button");
-
-
-            historyEl.append(newButton);
-            newButton.append(link);
-
-
-            newButton.style.textTransform = "capitalize";
-
-        }
-    }
-}
-
-renderHistory();
-
+formEl.addEventListener('submit', getCurrentWeather);
 
 searchButtonEl.addEventListener("click", getCurrentWeather);
-
-
-
